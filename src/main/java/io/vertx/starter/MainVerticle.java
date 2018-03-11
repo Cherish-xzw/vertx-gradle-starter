c6608b4961141e8eb7eb4ef811379d21545afb96
travisxu <xuzhengwei.amor@gmail.com>
2018-03-11 12:11:55 +0800

    Serve static resources

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,14 +1,20 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start() {
+    Router router =  Router.router(vertx);
+    router.route().handler(StaticHandler.create());
+
     vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
+        .requestHandler(router::accept)
         .listen(8080);
+    System.out.println("Server started at http://localhost:8080 ");
   }
 
 }
