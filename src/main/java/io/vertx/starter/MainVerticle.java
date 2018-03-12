dc23ad32e647e74d4716eee98850eae01a61046e
travisxu <xuzhengwei.amor@gmail.com>
2018-03-12 11:32:35 +0800

    Add template engine

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -3,17 +3,34 @@ package io.vertx.starter;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.templ.ThymeleafTemplateEngine;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start() {
     Router router =  Router.router(vertx);
-    router.route().handler(StaticHandler.create());
+    StaticHandler serve =  StaticHandler.create();
+    ThymeleafTemplateEngine engine = ThymeleafTemplateEngine.create();
+
+    router.route("/assets/*").handler(serve);
+    router.get("/").handler(ctx -> {
+      // we define a hardcoded title for our application
+      ctx.put("welcome", "Hi there!");
+      // and now delegate to the engine to render it.
+      engine.render(ctx,"templates/", "index.html", res -> {
+        if (res.succeeded()) {
+          ctx.response().end(res.result());
+        } else {
+          ctx.fail(res.cause());
+        }
+      });
+    });
 
     vertx.createHttpServer()
         .requestHandler(router::accept)
         .listen(8080);
+    System.setProperty("vertx.disableFileCaching", "true");
     System.out.println("Server started at http://localhost:8080 ");
   }
 
---
+++ src/main/resources/webroot/js/index.js
@@ -0,0 +1,6 @@
+console.log("script");
+document
+  .getElementsByTagName("button")[0]
+  .addEventListener("click", function() {
+    alert("hello vert.x");
+  });
